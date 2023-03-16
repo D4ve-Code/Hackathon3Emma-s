c185ea743e0afdd594ec4faec72d1fc4cfbfac5c
Alexandre Essaifi <alex-es-31@outlook.com>
2023-03-16 10:44:45 +0100

    added antutu route

--- backend/src/router.js
+++ backend/src/router.js
@@ -3,10 +3,13 @@ const express = require("express");
 const router = express.Router();
 
 const phoneControllers = require("./controllers/phoneControllers");
+const antutuControllers = require("./controllers/antutuControllers");
 
 // routes //
 router.get("/phones", phoneControllers.browse);
 router.post("/phones", phoneControllers.add);
+
+router.get("/antutu", antutuControllers.browse);
 // router.get("/items/:id", itemControllers.read);
 // router.put("/items/:id", itemControllers.edit);
 // router.post("/items", itemControllers.add);
